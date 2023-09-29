d4d0da78594da75f398adec33c1e026a8ae96b53
anonymousduckduckgoose <anonymousduckduckgoose@gmail.com>
2023-09-29 15:06:32 -0600

    async,redirects,monitoring,tests,doc

--- setup.sh
+++ setup.sh
@@ -1,13 +1,58 @@
 #!/bin/bash
 
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get -y install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+
+# Check if the docker GPG key already exists
+if [ ! -f /etc/apt/keyrings/docker.gpg ]; then
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+fi
+
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
 # Update package manager
-sudo apt update
+sudo apt-get update
 
-# Install Docker
-sudo apt install -y docker
+# install docker and friends
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-# Navigate to your Dockerfile directory
-cd ./build
+# upadte docker socket permissions
+sudo chmod 666 /var/run/docker.sock
 
-# Build & run your Docker image via compose
-docker-compose up --build
+# set build directory from current working directory
+export BUILD_DIRECTORY="$PWD/build"
+
+# Create a systemd service unit file for your Python script
+cat <<EOF | sudo tee /etc/systemd/system/favicon-fetcher.service
+[Unit]
+Description=Favicon Fetching API Service
+After=docker.service
+Requires=docker.service
+
+[Service]
+TimeoutStartSec=0
+Restart=unless-stopped
+EnvironmentFile=/etc/my_service_env.conf
+WorkingDirectory=$BUILD_DIRECTORY
+ExecStart=/usr/bin/docker compose up --build
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Reload the systemd daemon to recognize the new service
+sudo systemctl daemon-reload
+
+# Start the service
+sudo systemctl start favicon-fetcher.service
+
+# Enable the service to start on boot
+sudo systemctl enable favicon-fetcher.service
